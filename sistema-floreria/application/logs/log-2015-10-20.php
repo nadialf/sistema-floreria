0bb4ed063ccf97ab20bdc223d6648b5f3b01f726
marcos-flores <ferarymarcos_chivas@hotmail.com>
2015-10-20 10:03:40 -0500

    prueba2
    prueba

---
+++ sistema-floreria/application/logs/log-2015-10-20.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2015-10-20 17:01:37 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\sistema-floreria\sistema-floreria\system\database\drivers\mysql\mysql_driver.php 92
+ERROR - 2015-10-20 17:01:37 --> Unable to select database: sistema-floreria

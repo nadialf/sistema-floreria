70bd124bcfb5507d9172bd209861637286710510
nadialf <nadialbrs@hotmail.com>
2015-11-01 12:05:16 -0600

    Cambios domingo 01/nov

---
+++ sistema-floreria/application/logs/log-2015-11-01.php
@@ -0,0 +1,6 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2015-11-01 19:01:21 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\sistema-floreria\sistema-floreria\system\database\drivers\mysql\mysql_driver.php 92
+ERROR - 2015-11-01 19:01:21 --> Severity: Notice  --> mysql_pconnect():  C:\xampp\htdocs\sistema-floreria\sistema-floreria\system\database\drivers\mysql\mysql_driver.php 92
+ERROR - 2015-11-01 19:01:21 --> Session: HMAC mismatch. The session cookie data did not match what was expected.
+ERROR - 2015-11-01 19:01:21 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\xampp\htdocs\sistema-floreria\sistema-floreria\system\database\drivers\mysql\mysql_driver.php 92
